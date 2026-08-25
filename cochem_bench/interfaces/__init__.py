106b32dd472a40147b405f22c426ce7ad94cfbc4
ProfJJK <joshua.john.klaassen@gmail.com>
2026-08-24 22:29:56 -0500

    Automated completion of draft_task2_pt2_telemetry.md

--- cochem_bench/interfaces/__init__.py
+++ cochem_bench/interfaces/__init__.py
@@ -1,6 +1,29 @@
-from .webgl_perf import WebGLPerformanceTracker, benchmark_fps
+from .cochem_bench_telemetry import (
+    ContextCompressor,
+    InterceptionAlert,
+    NanInfInterceptor,
+    NDJSONStreamer,
+    StatisticalSummary,
+    TelemetryEvent,
+    decimate_lttb,
+    get_cochem_artifacts_dir,
+    get_element_mass_mendeleev,
+    get_logs_workspace_dir,
+    get_scratch_workspace_dir,
+)
+from .webgl_perf import WebGLPerformanceTracker
 
 __all__ = [
     "WebGLPerformanceTracker",
-    "benchmark_fps",
+    "ContextCompressor",
+    "NDJSONStreamer",
+    "NanInfInterceptor",
+    "StatisticalSummary",
+    "TelemetryEvent",
+    "InterceptionAlert",
+    "get_cochem_artifacts_dir",
+    "get_scratch_workspace_dir",
+    "get_logs_workspace_dir",
+    "get_element_mass_mendeleev",
+    "decimate_lttb",
 ]
